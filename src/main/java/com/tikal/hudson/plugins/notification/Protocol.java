322d44abaf02ff029ad28ceada59b596b3a9c27b
Rodrigo Lins de Oliveira <rodrigo.lins.de.oliveira@sap.com>
2017-01-05 17:00:54 +0100

    Added check for HTTP/http HTTPS/http and NO_PROXY/no_proxy

--- src/main/java/com/tikal/hudson/plugins/notification/Protocol.java
+++ src/main/java/com/tikal/hudson/plugins/notification/Protocol.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -58,38 +58,111 @@ public enum Protocol {
     HTTP {
         @Override
         protected void send(String url, byte[] data, int timeout, boolean isJson) throws IOException {
+
             URL targetUrl = new URL(url);
             if (!targetUrl.getProtocol().startsWith("http")) {
-              throw new IllegalArgumentException("Not an http(s) url: " + url);
+                throw new IllegalArgumentException("Not an http(s) url: " + url);
             }
 
-            // Verifying if the HTTP_PROXY is available
-            final String httpProxyUrl = System.getenv().get("http_proxy");
-            URL proxyUrl = null;
-            if (httpProxyUrl != null && httpProxyUrl.length() > 0) {
-              proxyUrl = new URL(httpProxyUrl);
-              if (!proxyUrl.getProtocol().startsWith("http")) {
-                throw new IllegalArgumentException("Not an http(s) url: " + httpProxyUrl);
-              }
+            // Verifying if the http_proxy or HTTP_PROXY is available
+            URL proxyURL = null;
+
+            // Verifying http_proxy URL
+            String http_proxyURL = System.getenv().get("http_proxy");
+
+            if (http_proxyURL != null && http_proxyURL.length() > 0) {
+                proxyURL = new URL(http_proxyURL);
             }
+
+            // Verifying HTTP_PROXY URL
+            String HTTP_PROXYURL = System.getenv("HTTP_PROXY");
+
+            if (HTTP_PROXYURL != null && HTTP_PROXYURL.length() > 0) {
+                proxyURL = new URL(HTTP_PROXYURL);
+            }
+
+            // Verifying https_proxy URL
+            String https_proxyURL = System.getenv("https_proxy");
+
+            if (https_proxyURL != null && https_proxyURL.length() > 0) {
+                proxyURL = new URL(https_proxyURL);
+            }
+
+            // Verifying HTTP_PROXY URL
+            String HTTPS_PROXYURL = System.getenv("HTTPS_PROXY");
+
+            if (HTTPS_PROXYURL != null && HTTPS_PROXYURL.length() > 0) {
+                proxyURL = new URL(HTTPS_PROXYURL);
+            }
+
+            // Verifying no_proxy URL
+            boolean no_proxy = false;
+
+            String no_proxyURL = System.getenv("no_proxy");
+
+            if (no_proxyURL != null && no_proxyURL.length() > 0) {
+                System.out.println("no_proxyURL = " + no_proxyURL);
+                final String[] excludedURLS = no_proxyURL.split(",");
+                for (String excludedURL : excludedURLS) {
+                    // The variable no_proxy handles the suffixes.
+                    if (targetUrl.getHost().endsWith(excludedURL.replaceFirst("^(\\*.|\\*|\\.)", ""))) {
+                        no_proxy = true;
+                        System.out.println("excludedURL = " + excludedURL);
+                    }
+                }
+            }
+
+            // Verifying NO_PROXY URL
+            String NO_PROXYURL = System.getenv("NO_PROXY");
+
+            if (NO_PROXYURL != null && NO_PROXYURL.length() > 0) {
+                System.out.println("NO_PROXYURL = " + NO_PROXYURL);
+                final String[] excludedURLS = NO_PROXYURL.split(",");
+                for (String excludedURL : excludedURLS) {
+                    // The variable no_proxy handles the suffixes.
+                    if (targetUrl.getHost().endsWith(excludedURL.replaceFirst("^(\\*.|\\*|\\.)", ""))) {
+                        no_proxy = true;
+                        System.out.println("excludedURL = " + excludedURL);
+                    }
+                }
+            }
+            System.out.println("PROXY URL: "+ proxyURL);
+            System.out.println("TARGET URL: " + targetUrl);
+            System.out.println("NO_PROXY: " + no_proxy);
 
             HttpURLConnection connection = null;
-            if (proxyUrl == null) {
-              connection = (HttpURLConnection) targetUrl.openConnection();
-
+            if (proxyURL == null || no_proxy) {
+                System.out.println("No proxy or no_proxy/NO_PROXY configuration was found");
+                connection = (HttpURLConnection) targetUrl.openConnection(Proxy.NO_PROXY);
             } else {
-              // Proxy connection to the address provided
-              final int proxyPort = proxyUrl.getPort() > 0 ? proxyUrl.getPort() : 80;
-              Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(), proxyPort));
-              connection = (HttpURLConnection) targetUrl.openConnection(proxy);
+                // Proxy connection to the address provided
+
+                // Test for HTTP or http proxy
+                if(proxyURL.getProtocol().equals("http")) {
+                    System.out.println("Using http/HTTP proxy");
+                    final int proxyPort = proxyURL.getPort() > 0 ? proxyURL.getPort() : 80;
+                    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyURL.getHost(), proxyPort));
+                    connection = (HttpURLConnection) targetUrl.openConnection(proxy);
+                }
+                // Test for HTTPS of https proxy
+                else if(proxyURL.getProtocol().equals("https")) {
+                    System.out.println("Using https/HTTPS proxy");
+                    final int proxyPort = proxyURL.getPort() > 0 ? proxyURL.getPort() : 443;
+                    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyURL.getHost(), proxyPort));
+                    connection = (HttpURLConnection) targetUrl.openConnection(proxy);
+                }
+                // If not HTTP or HTTPS
+                else {
+                    throw new IllegalArgumentException("Not an http(s) url: " + proxyURL);
+                }
             }
 
-            connection.setRequestProperty("Content-Type", String.format( "application/%s;charset=UTF-8", isJson ? "json" : "xml" ));
+            connection.setRequestProperty("Content-Type", String.format("application/%s;charset=UTF-8", isJson ? "json" : "xml"));
             String userInfo = targetUrl.getUserInfo();
             if (null != userInfo) {
-              String b64UserInfo = DatatypeConverter.printBase64Binary(userInfo.getBytes());
-              String authorizationHeader = "Basic " + b64UserInfo;
-              connection.setRequestProperty("Authorization", authorizationHeader);
+                String b64UserInfo = DatatypeConverter.printBase64Binary(userInfo.getBytes());
+                String authorizationHeader = "Basic " + b64UserInfo;
+                connection.setRequestProperty("Authorization", authorizationHeader);
             }
             connection.setFixedLengthStreamingMode(data.length);
             connection.setDoInput(true);
@@ -98,38 +171,38 @@ public enum Protocol {
             connection.setReadTimeout(timeout);
             connection.connect();
             try {
-              OutputStream output = connection.getOutputStream();
-              try {
-                output.write(data);
-                output.flush();
-              } finally {
-                output.close();
-              }
+                OutputStream output = connection.getOutputStream();
+                try {
+                    output.write(data);
+                    output.flush();
+                } finally {
+                    output.close();
+                }
             } finally {
-              // Follow an HTTP Temporary Redirect if we get one,
-              //
-              // NB: Normally using the HttpURLConnection interface, we'd call
-              // connection.setInstanceFollowRedirects(true) to enable 307 redirect following but
-              // since we have the connection in streaming mode this does not work and we instead
-              // re-direct manually.
-              if (307 == connection.getResponseCode()) {
-                String location = connection.getHeaderField("Location");
-                connection.disconnect();
-                send(location, data,timeout, isJson);
-              } else {
-                connection.disconnect();
-              }
+                // Follow an HTTP Temporary Redirect if we get one,
+                //
+                // NB: Normally using the HttpURLConnection interface, we'd call
+                // connection.setInstanceFollowRedirects(true) to enable 307 redirect following but
+                // since we have the connection in streaming mode this does not work and we instead
+                // re-direct manually.
+                if (307 == connection.getResponseCode()) {
+                    String location = connection.getHeaderField("Location");
+                    connection.disconnect();
+                    send(location, data, timeout, isJson);
+                } else {
+                    connection.disconnect();
+                }
             }
         }
 
         @Override
-        public void validateUrl( String url ) {
+        public void validateUrl(String url) {
             try {
                 // noinspection ResultOfObjectAllocationIgnored
-                new URL( url );
-            } catch ( MalformedURLException e ) {
-                throw new RuntimeException( String.format( "%sUse http://hostname:port/path for endpoint URL",
-                                                           isEmpty ( url ) ? "" : "Invalid URL '" + url + "'. " ));
+                new URL(url);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(String.format("%sUse http://hostname:port/path for endpoint URL",
+                        isEmpty(url) ? "" : "Invalid URL '" + url + "'. "));
             }
         }
     };
@@ -144,12 +217,12 @@ public enum Protocol {
                 throw new Exception();
             }
         } catch (Exception e) {
-            throw new RuntimeException( String.format( "%sUse hostname:port for endpoint URL",
-                                                       isEmpty ( url ) ? "" : "Invalid URL '" + url + "'. " ));
+            throw new RuntimeException(String.format("%sUse hostname:port for endpoint URL",
+                    isEmpty(url) ? "" : "Invalid URL '" + url + "'. "));
         }
     }
 
-    private static boolean isEmpty( String s ) {
-        return (( s == null ) || ( s.trim().length() < 1 ));
+    private static boolean isEmpty(String s) {
+        return ((s == null) || (s.trim().length() < 1));
     }
 }
